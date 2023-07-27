3b6b27d5770f8dd6d92153a59a3b6d9ed1cfbef4
Panos Oikonomou <panos.economou.v@gmail.com>
2023-07-27 21:02:30 +0400

    Currently Empty

---
+++ src/ActionInitialization.cc
@@ -0,0 +1,38 @@
+/************************
+•     ┓             •    
+┓┏┳┓┏┓┃┏┓┏┳┓┏┓┏┓╋┏┓╋┓┏┓┏┓
+┗┛┗┗┣┛┗┗ ┛┗┗┗ ┛┗┗┗┻┗┗┗┛┛┗
+    ┛                    
+  Action Initialization
+************************/
+
+// Import the header file
+#include "ActionInitialization.hh"
+
+
+// Constructor
+ActionInitialization::ActionInitialization()
+{}
+
+//-----------------------8<-------------[ cut here ]------------------------
+
+// Destructor
+ActionInitialization::~ActionInitialization()
+{}
+
+//-----------------------8<-------------[ cut here ]------------------------
+
+// Build for Master
+void ActionInitialization::BuildForMaster() const
+{
+    // SetUserAction(new RunAction())
+}
+
+//-----------------------8<-------------[ cut here ]------------------------
+
+// Build for Master
+void ActionInitialization::Build() const
+{
+    // SetUserAction(new RunAction())
+}
+
