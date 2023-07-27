e14a4741264a88575d9f4946f9accffada92e9c6
Panos Oikonomou <panos.economou.v@gmail.com>
2023-07-27 20:53:54 +0400

    Physics List With Radiactive Decay and Optics

---
+++ src/PhysicsList.cc
@@ -0,0 +1,24 @@
+/************************
+•     ┓             •    
+┓┏┳┓┏┓┃┏┓┏┳┓┏┓┏┓╋┏┓╋┓┏┓┏┓
+┗┛┗┗┣┛┗┗ ┛┗┗┗ ┛┗┗┗┻┗┗┗┛┛┗
+    ┛                    
+       Physics List
+************************/
+
+#include "PhysicsList.hh" 
+
+// Constructor
+PhysicsList::PhysicsList() : G4VModularPhysicsList()
+{
+	RegisterPhysics (new G4EmStandardPhysics());
+	RegisterPhysics (new G4OpticalPhysics());
+	RegisterPhysics (new G4DecayPhysics());
+	RegisterPhysics (new G4RadioactiveDecayPhysics());
+}
+
+//-----------------------8<-------------[ cut here ]------------------------
+
+// Destructor
+PhysicsList::~PhysicsList()
+{}
