20bc4e0f0e07ac4ded7d9d3db450363a4cf8e27a
Panos Oikonomou <po524@nyu.edu>
2023-07-31 16:05:41 +0400

    Relevant to run in hpc

---
+++ hpc/run_start.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Number of tasks 
+#SBATCH --ntasks 1024 
+# Set Memory
+#SBATCH --mem 64GB
+# Set WALLTIME
+#SBATCH --time 05:00:00
+# Set output and error files
+#SBATCH -o start-job.%J.out
+#SBATCH -e start-job.%J.err
+# Send email when done
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=po524@nyu.edu
+
+#----------------------------------#
+#------------EXECUTION-------------#
+#----------------------------------#
+
+# Load modules
+module purge
+source load_modules.sh
+
+# Run the application
+cd /scratch/po524/apxs/build/
+start_time=`date +%s`
+mpiexec -n 1024 ./apxs ./macros/start.mac
+end_time=`date +%s`
+echo execution time was `expr $end_time - $start_time` s.
+echo "SIM IS DONE!"
+# echo "MERGING!"
+# ../hpc/merge.sh ./output > ./output/merge.csv
