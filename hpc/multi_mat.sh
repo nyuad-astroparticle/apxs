44cf281f1dc2fc8478444d5c6e28cb5a126178a3
Isaac Belew Sarnoff <ibs257@login3.fast>
2023-09-11 15:33:24 +0400

    something is amiss with the output files

--- hpc/multi_mat.sh
+++ hpc/multi_mat.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Number of tasks 
-#SBATCH --ntasks 30 
+#SBATCH --ntasks 1000 
 # Set Memory
 #SBATCH --mem 64GB
 # Set WALLTIME
-#SBATCH --time 15:00:00
+#SBATCH --time 30:00:00
 # Set output and error files
 #SBATCH -o start-job.%J.out
 #SBATCH -e start-job.%J.err
@@ -21,12 +21,15 @@ module purge
 source load_modules.sh
 
 # Run the application
-cd /scratch/ibs257/apxs/build2/
+cd /scratch/ibs257/apxs/build/
 start_time=`date +%s`
-mpiexec -n 30 ./apxs ./macros/toy_hpc.mac
+mpiexec -n 1000 ./apxs ./macros/multi_mat.mac
 end_time=`date +%s`
 echo execution time was `expr $end_time - $start_time` s.
 echo "SIM IS DONE!"
 # echo "MERGING!"
 # ../hpc/merge.sh ./output > ./output/merge.csv
+
+
+
 
