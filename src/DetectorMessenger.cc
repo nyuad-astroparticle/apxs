7ac6bb3412e41df6acae8da017b8f5c422098f9e
F7East <tengiz.ibrayev@gmail.com>
2024-02-06 10:57:04 +0400

    added a tilt, air as a background and more verbosity for commands.

--- src/DetectorMessenger.cc
+++ src/DetectorMessenger.cc
@@ -33,40 +33,50 @@ DetectorMessenger::DetectorMessenger(DetectorConstruction* detectorConstruction)
     directory->SetGuidance("Manages the materials assigned to different parts of the setup.");
 
     // COMMANDS --------------------------------------------------------------
-    // Change the source material
-    setSourceMaterial = new G4UIcmdWithAString("/apxs/setSourceMaterial",this);
-    setSourceMaterial->SetGuidance("Change the material of the source into any of the predefined ones.");
-    setSourceMaterial->SetParameterName("Material",false);
-    setSourceMaterial->AvailableForStates(G4State_PreInit,G4State_Idle);
+    // Change the source material currently not used
+        setSourceMaterial               = new G4UIcmdWithAString("/apxs/setSourceMaterial",this);
+        setSourceMaterial               ->SetGuidance("Change the material of the source into any of the predefined ones.");
+        setSourceMaterial               ->SetParameterName("Material",false);
+        setSourceMaterial               ->AvailableForStates(G4State_PreInit,G4State_Idle);
 
-    setSourceVolume = new G4UIcmdWithAString("/apxs/setSourceVolume",this);
-    setSourceVolume->SetGuidance("Change which disk is the source");
-    setSourceVolume->SetParameterName("Disk volume",false);
-    setSourceVolume->AvailableForStates(G4State_PreInit,G4State_Idle);
+    // Select which volume should be the source at the moment
+        setSourceVolume                 = new G4UIcmdWithAString("/apxs/setSourceVolume",this);
+        setSourceVolume                 ->SetGuidance("Change which disk is the source");
+        setSourceVolume                 ->SetParameterName("Disk volume",false);
+        setSourceVolume                 ->AvailableForStates(G4State_PreInit,G4State_Idle);
     
-    setSourceMaterialAndName = new G4UIcommand("/apxs/setSourceMaterialAndName", this);
-    setSourceMaterialAndName->SetGuidance("Change the material of the source and set the source name.");
-    setSourceMaterialAndName->SetGuidance("This command requires two parameters: Material and SourceName.");
+    // Set the material of a picked volume
+        setSourceMaterialAndName        = new G4UIcommand("/apxs/setSourceMaterialAndName", this);
+        setSourceMaterialAndName        ->SetGuidance("Change the material of the source and set the source name.");
+        setSourceMaterialAndName        ->SetGuidance("This command requires two parameters: Material and SourceName.");
 
-    G4UIparameter* materialParam = new G4UIparameter("Material", 's', false);
-    materialParam->SetGuidance("Material name.");
-    setSourceMaterialAndName->SetParameter(materialParam);
+        G4UIparameter* materialParam    = new G4UIparameter("Material", 's', false);
+        materialParam                   ->SetGuidance("Material name.");
+        setSourceMaterialAndName        ->SetParameter(materialParam);
 
-    G4UIparameter* sourceNameParam = new G4UIparameter("SourceName", 's', false);
-    sourceNameParam->SetGuidance("Source name.");
-    setSourceMaterialAndName->SetParameter(sourceNameParam);
+        G4UIparameter* sourceNameParam  = new G4UIparameter("SourceName", 's', false);
+        sourceNameParam                 ->SetGuidance("Source name.");
+        setSourceMaterialAndName        ->SetParameter(sourceNameParam);
 
-    setSourceMaterialAndName->AvailableForStates(G4State_PreInit, G4State_Idle);
+        setSourceMaterialAndName        ->AvailableForStates(G4State_PreInit, G4State_Idle);
 
-    setTargetMaterial = new G4UIcmdWithAString("/apxs/setTargetMaterial",this);
-    setTargetMaterial->SetGuidance("Change the material of the Target into any of the predefined ones.");
-    setTargetMaterial->SetParameterName("Material",false);
-    setTargetMaterial->AvailableForStates(G4State_PreInit,G4State_Idle);
+    // Set target material duh
+        setTargetMaterial               = new G4UIcmdWithAString("/apxs/setTargetMaterial",this);
+        setTargetMaterial               ->SetGuidance("Change the material of the Target into any of the predefined ones.");
+        setTargetMaterial               ->SetParameterName("Material",false);
+        setTargetMaterial               ->AvailableForStates(G4State_PreInit,G4State_Idle);
 
-    createMultipleSources = new G4UIcmdWithAString("/apxs/createMultipleSources", this);
-    createMultipleSources->SetGuidance("Add a number of disks around the detector");
-    createMultipleSources->SetParameterName("Number", false);
-    createMultipleSources->AvailableForStates(G4State_PreInit,G4State_Idle);
+    // Add a number of disk sources symmetrically spread around the circle
+        createMultipleSources           = new G4UIcmdWithAString("/apxs/createMultipleSources", this);
+        createMultipleSources           ->SetGuidance("Add a number of disks around the detector");
+        createMultipleSources           ->SetParameterName("Number", false);
+        createMultipleSources           ->AvailableForStates(G4State_PreInit,G4State_Idle);
+
+    // Tilt the target surface
+        setTiltAngle = new G4UIcmdWithAString("/apxs/setTiltAngle",this);
+        setTiltAngle->SetGuidance("Title the target below about x axis by this angle in degrees");
+        setTiltAngle->SetParameterName("Angle", false);
+        setTiltAngle->AvailableForStates(G4State_PreInit, G4State_Idle);
 
 }
 
@@ -110,5 +120,9 @@ void DetectorMessenger::SetNewValue(G4UIcommand* command, G4String value)
     if (command == setSourceVolume)
     {
         detectorConstruction->setSourceVolume(value);
+    }
+    if (command == setTiltAngle)
+    {
+        detectorConstruction->tiltTarget(std::stoi(value));
     }
 }
