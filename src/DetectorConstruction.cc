0afd8b3bd6f9957de9ddbb5283961c4d723c4f16
Tengiz Ibrayev <tengizibrayev@Tengizs-MacBook-Pro-2.local>
2025-07-28 23:08:31 +1000

    added aux info processing for colors and colored volumes

--- src/DetectorConstruction.cc
+++ src/DetectorConstruction.cc
@@ -188,11 +188,70 @@ G4VPhysicalVolume* DetectorConstruction::Construct()
     #ifdef DETECTOR_GDML
     // Start the parser
     G4GDMLParser* parser = new G4GDMLParser();
+    parser->Read(filename,false);
+    
+    G4VisAttributes* invisible  = new G4VisAttributes(G4Colour(1.0, 1.0, 1.0));
+    invisible->SetVisibility(false);
+    
+	// Example how to retrieve Auxiliary Information for sensitive detector taken directly from G04 example 
+
+	const G4GDMLAuxMapType* auxmap = parser->GetAuxMap();
+	G4cout << "Found " << auxmap->size()
+			<< " volume(s) with auxiliary information."
+			<< G4endl << G4endl;
+
+	for (G4GDMLAuxMapType::const_iterator iter = auxmap->begin(); iter != auxmap->end(); iter++) 
+	{
+		G4cout << "Volume " << ((*iter).first)->GetName()
+			<< " has the following list of auxiliary information: "
+			<< G4endl << G4endl;
+
+		G4double r = 1.0, g = 1.0, b = 1.0; // Default color values
+		G4double opacity = 1.0; // Default opacity value
+
+		for (G4GDMLAuxListType::const_iterator vit = (*iter).second.begin(); vit != (*iter).second.end(); vit++)
+		{
+			G4cout << "--> Type: " << (*vit).type << " Value: " << (*vit).value << G4endl;
+			
+			// Check if the auxiliary information is for color
+			if ((*vit).type == "Color") 
+			{
+				// Parse the color string into three separate float values
+				std::istringstream iss((*vit).value);
+				if (!(iss >> r >> g >> b)) 
+				{
+					G4cerr << "Error parsing color string: " << (*vit).value << G4endl;
+				}
+			}
+			// Check if the auxiliary information is for opacity
+			else if ((*vit).type == "opacity") 
+			{
+				// Parse the opacity string into a single float value
+				std::istringstream iss((*vit).value);
+				if (!(iss >> opacity)) 
+				{
+					G4cerr << "Error parsing opacity string: " << (*vit).value << G4endl;
+				}
+			}
+		}
+
+		// Create a G4Colour object with the parsed values
+		G4Colour colour(r, g, b, opacity);
+
+		// Create and set the visualization attributes
+		G4VisAttributes* visAttributes = new G4VisAttributes(colour);
+		visAttributes->SetVisibility(true);
+
+		// Assign the visualization attributes to the volume
+		((*iter).first)->SetVisAttributes(visAttributes);
+	}
+
+	G4cout << G4endl;
+
 
     // Read the file 
     // (the `false` flag turns off the check to make sure the GDML file is properly formatted)
     
-    parser->Read(filename,false);
 
     if (parser->IsValid("detectLogical"))
     {
