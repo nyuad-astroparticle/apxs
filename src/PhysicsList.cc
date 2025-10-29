7fbf5a15899846e57149ebb2fa41b08400b213ff
Tengiz Ibrayev <tengiz@agqhcqjdw32.tail819d22.ts.net>
2025-10-29 15:17:51 +0400

    different physics list, more events

--- src/PhysicsList.cc
+++ src/PhysicsList.cc
@@ -10,8 +10,8 @@
 #include "PhysicsList.hh" 
 
 // Geant4 Physics Lists Constructors we are using
-// #include "G4EmStandardPhysics.hh"
-#include "G4EmLivermorePhysics.hh"
+#include "G4EmStandardPhysics.hh"
+// #include "G4EmLivermorePhysics.hh"
 // #include "G4EmPenelopePhysics.hh"
 #include "G4OpticalPhysics.hh"
 #include "G4DecayPhysics.hh"
@@ -23,8 +23,8 @@
 // Constructor
 PhysicsList::PhysicsList() : G4VModularPhysicsList()
 {
-	// RegisterPhysics (new G4EmStandardPhysics());
-	RegisterPhysics (new G4EmLivermorePhysics());
+	RegisterPhysics (new G4EmStandardPhysics());
+	// RegisterPhysics (new G4EmLivermorePhysics());
 	// RegisterPhysics (new G4EmPenelopePhysics());
 	// RegisterPhysics (new G4OpticalPhysics());
 	RegisterPhysics (new G4DecayPhysics());
