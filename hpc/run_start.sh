c88f484899f46703227cd545cdc810bcaaf076fe
Tengiz Ibrayev <tengiz@agqhcqjdw32.tail819d22.ts.net>
2025-10-29 15:26:52 +0400

    tailored to Sandhya

--- hpc/run_start.sh
+++ hpc/run_start.sh
@@ -1,29 +1,26 @@
 #!/bin/bash
-# Number of tasks 
-#SBATCH --ntasks 2000 
+# Number of tasks
+#SBATCH --ntasks 4096
 # Set Memory
 #SBATCH --mem 64GB
 # Set WALLTIME
-#SBATCH --time 15:00:00
+#SBATCH --time 5:00:00
 # Set output and error files
 #SBATCH -o start-job.%J.out
 #SBATCH -e start-job.%J.err
 # Send email when done
 #SBATCH --mail-type=END,FAIL
-#SBATCH --mail-user=ibs257@nyu.edu
-
+#SBATCH --mail-user=ss14729@nyu.edu
 #----------------------------------#
 #------------EXECUTION-------------#
 #----------------------------------#
-
 # Load modules
 module purge
 source load_modules.sh
-
 # Run the application
-cd /scratch/ibs257/apxs/build/
+cd /scratch/ss14729/geant4/apxs/build/
 start_time=`date +%s`
-mpiexec -n 3000 ./apxs ./macros/ferroannoriticanorthosite.mac
+mpiexec -n 4096 ./apxs -g ./geometry/new_geom/output.gdml -m ./macros/test_new_geom.mac
 end_time=`date +%s`
 echo execution time was `expr $end_time - $start_time` s.
 echo "SIM IS DONE!"
