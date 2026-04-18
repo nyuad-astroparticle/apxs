3580d43d233b4147f1ea67635e193432b330e665
Sandhya Sharma <ss14729@dn306.fast>
2026-04-18 15:04:03 +0400

    working version of lxs-dwarf

--- hpc/run_start.sh
+++ hpc/run_start.sh
@@ -23,10 +23,10 @@ source load_modules.sh
 # module load gcc cmake mpich expat
 
 # Run the application
-cd /scratch/ss14729/apxs/build1/
+cd /scratch/ss14729/apxs/build/
 start_time=`date +%s`
 # mpiexec -n 2048 ./apxs -g ./geometry/new_geom/output.gdml -m ./macros/test_new_geom.mac > /dev/null 2>&1
-mpiexec -n 2048 ./apxs -g ./geometry/new_geom/outlxs.gdml -m ./macros/test_new_geom.mac 
+mpiexec -n 2048 ./apxs -g ./geometry/new_geom/outlxs1.gdml -m ./macros/test_new_geom.mac 
 end_time=`date +%s`
 echo execution time was `expr $end_time - $start_time` s.
 echo "SIMULATION COMPLETED!"
--- output.sh
+++ output.sh
@@ -4,7 +4,7 @@
 
 set -euo pipefail
 
-cd /scratch/ss14729/apxs/build_0/output
+cd /scratch/ss14729/apxs/build/output
 
 if [[ $# -ne 1 ]]; then
   echo "Usage: $0 <new_dir_name>"
